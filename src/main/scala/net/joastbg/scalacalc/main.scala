85b73f85b81b663c5069d869dfe99739c4a1f137
Johan Astborg <joastbg@gmail.com>
2013-03-13 12:45:16 +0100

    Replaced tabs with spaces

--- src/main/scala/net/joastbg/scalacalc/main.scala
+++ src/main/scala/net/joastbg/scalacalc/main.scala
@@ -2,17 +2,17 @@ package net.joastbg.scalacalc
 
 object ScalaCalc extends App {
 
-	// Init variables
+  // Init variables
   val vals = Map[String, Double]("x" -> 4, "y" -> 3)
-	vals.foreach(p => println(p._1 + "=" + p._2))
+  vals.foreach(p => println(p._1 + "=" + p._2))
 
   // Parse expression
   val str = "12.0*2.5 + x*(y+50)*1.23"
   val expr = ExprParser.apply(str)
 
-	// Evaluate expression
-	val res = ExprEval.eval(expr, vals)
+  // Evaluate expression
+  val res = ExprEval.eval(expr, vals)
 
-	println("Expression => " + str)
-	println("Equals => " + res)
+  println("Expression => " + str)
+  println("Equals => " + res)
 }
